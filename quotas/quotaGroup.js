300db57a1e6ffdddb414905f5998e2e1b01ba06e
KGSDilshan <dilshang@kgsresearch.com>
2020-03-25 15:24:24 -0400

    updated stub to consider more details about nSize

--- quotas/quotaGroup.js
+++ quotas/quotaGroup.js
@@ -1,14 +1,27 @@
 class QuotaGroup {
-    constructor(name, config, subquotas) {
+    constructor(name, config, subQuotas) {
         this.group_name = name;
         this.isTri = config.isTri;
         this.isDual = config.isDual;
         this.isFlex = config.isFlex;
         this.isRawFlex = config.isRawFlex;
         this.flexAmount = config.flexAmount;
-        this.nSize = config.nSizes;
+        this.nSizes = config.nSizes.slice();
+        this.totalN = this.nSizes.reduce((a, b) => a + b, 0);
         this.subQuotas = [];
         this.rawSubQuotas = subQuotas.slice();
+        this.mode = 0;
+        if (this.isTri) {
+            this.mode = 3;
+        } else if (this.isDual) {
+            this.mode = 2;
+        } else {
+            this.mode = 1;
+        }
+        if (this.nSize.length < this.mode) {
+            alert("Quota group " + this.group_name + " is mode " + this.mode.toString() +
+             ", but N Sizes input only contains " + this.nSizes.length.toString());
+        }
     }
 
     get_name() {
