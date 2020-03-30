bd319e04cd7cc82dc4d49545da937e5c6f394fd3
KGSDilshan <dilshang@kgsresearch.com>
2020-03-30 12:18:49 -0400

    add clients file

---
+++ quotas/customClients.js
@@ -0,0 +1,84 @@
+class ALClient extends BaseClient {
+    constructor() {
+        super("AL");
+    }
+
+    clientSpecificWarnings() {
+        if (RAN_CSWARNINGS)
+            return;
+        RAN_CSWARNINGS = true;
+        // if all quotas except splits were not inactive, warn
+        for (let i = 0; i < QUOTA_GROUPS.length; i++) {
+            for (let j = 0; j < QUOTA_GROUPS[i].subQuotas.length; j++) {
+                let q = QUOTA_GROUPS[i].subQuotas[j];
+                if (q.active == true && !q.name.toLowerCase().includes("split")) {
+                    // warn quota is active
+                    QUOTA_GROUPS[i].warnings.push("WARNING: " + q.name + ", quota is active. Made inactive (Checklist)");
+                    q.active = false;
+                }
+            }
+        }
+        // check if Gender has split quotas
+        let checked = false;
+        for (let i = 0; i < QUOTA_GROUPS.length; i++) {
+            if (QUOTA_GROUPS[i].getName().toLowerCase().includes("gender") || QUOTA_GROUPS[i].getName().toLowerCase().includes("sex")) {
+                checked = true;
+                if (!QUOTA_GROUPS[i].hasSplits) {
+                    console.log(QUOTA_GROUPS[i], "missing gender split");
+                    QUOTA_GROUPS[i].warnings.push("WARNING: Gender Quota missing split quota");
+                    break;
+                }
+            }
+        }
+        if (checked == false && QUOTA_GROUPS.length > 0) {
+            QUOTA_GROUPS[0].warnings.push("WARNING: Gender Quota not defined");
+        }
+    }
+}
+
+
+class DBClient extends BaseClient {
+    constructor() {
+        super("DB");
+    }
+
+    clientSpecificWarnings() {
+        if (RAN_CSWARNINGS)
+            return;
+        RAN_CSWARNINGS = true;
+
+        // Gender codes are reversed
+        /*
+        - If gender starts with an "m", it's male
+        - If gender starts with an "f", it's female
+        */
+
+        // Balance all quotas by splits except Geo, Lang, Phonetype
+
+        // Phone Type 50% LL, 50% Cell
+
+        // Add other offsetter for ethnicity quota
+
+        // Presidential vote quota is inactive (??? - perhaps have the programmer add this in?)
+    }
+}
+
+
+class FMClient extends BaseClient {
+    constructor() {
+        super("DB");
+    }
+
+    clientSpecificWarnings() {
+        if (RAN_CSWARNINGS)
+            return;
+        RAN_CSWARNINGS = true;
+
+        // if dual mode, n sizes should be equal
+        // if mode is phone, add 5% flex to all quotas that aren't counters
+        // phone type quotas should be inactive
+        // LL Min 30%
+        // Splits should be +5 added
+
+    }
+}
